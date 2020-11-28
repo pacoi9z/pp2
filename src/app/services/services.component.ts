b0a6e6e6311101326b36eb72dd8ce4fa6c5627e4
pacoi9z <ziane.ia@outlook.com>
2020-11-28 15:10:14 +0100

    ajout des comosant services au route

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -11,12 +11,29 @@ import { ActivatedRoute } from '@angular/router';
 export class ServicesComponent implements OnInit {
 
   
+  rendezvous = false;
+  certificascolarite = false;
+  absence = false;
+  retard = false;
+  medicament = false;
 
   constructor(private route:ActivatedRoute) { }
-  
+
 
   ngOnInit(): void {
-   this.route.params.subscribe(data => { console.log(data) });
+   this.route.params.subscribe(data => { 
+    this.rendezvous = false;
+    this.certificascolarite = false;
+    this.absence = false;
+    this.retard = false;
+    this.medicament = false;
+     if(data.serv === 'medicament') this.medicament = true;
+     else if(data.serv === 'retard') this.retard = true;
+     else if(data.serv === 'absence') this.absence = true;
+     else if(data.serv === 'certificascolarite') this.certificascolarite = true;
+     else if(data.serv === 'rendezvous') this.rendezvous = true;
+
+   });
   }
 
 }
